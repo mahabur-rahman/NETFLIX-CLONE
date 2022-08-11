dda90c5d032558d76329bb57fc6ad898d863d497
mahabur  rahman <annur4395@gmail.com>
2022-08-11 14:08:11 +0600

    user register api call

--- client/src/pages/Register/Register.js
+++ client/src/pages/Register/Register.js
@@ -1,21 +1,43 @@
-import { useRef } from "react";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { useHistory } from "react-router-dom";
 import "./register.scss";
+import axios from "axios";
 
 export default function Register() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+
+  const history = useHistory();
+
+  console.log(email, password, username);
 
   const emailRef = useRef();
   const passwordRef = useRef();
+  const usernameRef = useRef();
 
   const handleStart = () => {
     setEmail(emailRef.current.value);
   };
-  const handleFinish = () => {
-    setPassword(passwordRef.current.value);
-  };
 
+  const handleFinish = async (e) => {
+    e.preventDefault();
+
+    setPassword(passwordRef.current.value);
+    setUsername(usernameRef.current.value);
+
+    try {
+      await axios.post("auth/register", {
+        email,
+        username,
+        password,
+      });
+
+      history.push("/login");
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div className="register">
       <div className="top">
@@ -43,6 +65,7 @@ export default function Register() {
           </div>
         ) : (
           <form className="input">
+            <input type="text" placeholder="username" ref={usernameRef} />
             <input type="password" placeholder="password" ref={passwordRef} />
             <button className="registerButton" onClick={handleFinish}>
               Start
